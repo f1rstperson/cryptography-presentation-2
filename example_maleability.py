9d1c99c3597f79fd8701670d1b8c7c95fdbdaf12
f1p <f1rstperson@gmx.net>
2020-05-24 22:58:33 +0200

    example for simple cbc maleability

---
+++ example_maleability.py
@@ -0,0 +1,39 @@
+from cbc import *
+
+"""
+This file demonstrates the maleability of the CBC mode of operation in its most
+simple form.
+"""
+
+KEY = 187
+print("")
+
+# ----------------------------------------------------------------------
+# sender
+# ----------------------------------------------------------------------
+plain = [0b010111100101, 0b110010100010, 0b000000000000]
+
+cipher = cbc_encrypt(plain, KEY)
+
+# ----------------------------------------------------------------------
+# attacker
+# known: plain[1], cipher
+# ----------------------------------------------------------------------
+
+plain_1_goal          = 0b101010101010
+
+print("cipher before modification: " + str(bin(cipher[0])))
+
+# cipher[0] = plain_1_goal ^ cipher_1_intermediate # the modification!
+cipher[0] = cipher[0] ^ plain[1] ^ plain_1_goal
+
+print("cipher after modification : " + str(bin(cipher[0])))
+
+# ----------------------------------------------------------------------
+# receiver
+# ----------------------------------------------------------------------
+
+plain_rec = cbc_decrypt(cipher, KEY)
+print("decryption done by receiver: " + str([ bin(p) for p in plain_rec ]))
+
+if plain_rec[1] == plain_1_goal: print("plain[1] successfully manipulated")
