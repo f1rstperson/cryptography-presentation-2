44fdbb986a690495d8670c3e19e326f5d1c8f519
f1p <firstperson@gmx.net>
2020-06-01 21:25:07 +0200

    more readable example (no change in functionality)

--- example_maleability.py
+++ example_maleability.py
@@ -1,5 +1,46 @@
 from cbc import *
 
+# Allow for anonymous object creation
+Object = lambda **kwargs: type("Object", (), kwargs)
+
+class CBCGadget:
+    position: int
+    plain_1: int
+    cipher_0: int
+    cipher_1: int
+
+def replace_plaintext(
+        cipher: List[int], new_plaintext: str,
+        gadget: CBCGadget,
+) -> None:
+    """
+    Every step in cbc looks like
+
+                     plain[i] = cipher[i] ^ decrypt(cipher[i + 1])           (1)
+
+    Taking the inverse of XOR, we can therefore assert that
+
+                     decrypt(cipher[i + 1]) = plain[i] ^ cipher[i].          (2)
+
+    XORing any value with itself returns 0. Therefore
+
+                  0 = decrypt(cipher[i + 1]) ^ (plain[i] ^ cipher[i]).       (3)
+
+    XORing 0 with any value returns that value. Therefore
+
+                     new_plaintext = decrypt(cipher[i + 1])
+                                     ^ (plain[i] ^ cipher[i])                (4)
+                                     ^ new_plaintext.
+
+    Combining (1) and (4) (and knowing that XOR is commutative), we can now
+    manipulate cipher[i] to be `(plain[i] ^ cipher[i]) ^ new_plaintext`.
+    """
+
+    cipher[gadget.position] = gadget.plain_1 ^ gadget.cipher_0 ^ new_plaintext
+    return cipher
+
+
+
 """
 This file demonstrates the maleability of the CBC mode of operation in its most
 simple form.
@@ -22,11 +63,15 @@ cipher = cbc_encrypt(plain, KEY)
 
 plain_1_goal          = 0b101010101010
 
-print("cipher before modification: " + str(bin(cipher[0])))
+print("cipher before modification: " + str(bin(cipher[1])))
 
-cipher[1] = cipher[1] ^ plain[1] ^ plain_1_goal
+# cipher[1] = cipher[1] ^ plain[1] ^ plain_1_goal
+replace_plaintext(cipher, plain_1_goal, Object(
+    position = 1, cipher_0 = cipher[1],
+    cipher_1 = cipher[2], plain_1 = plain[1]
+))
 
-print("cipher after modification : " + str(bin(cipher[0])))
+print("cipher after modification : " + str(bin(cipher[1])))
 
 # ----------------------------------------------------------------------
 # receiver
